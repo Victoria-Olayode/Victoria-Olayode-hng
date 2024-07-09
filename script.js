fc5e37eae678201ac968f28705c115e1d1fd6eac
Victoria-Olayode <127393671+Victoria-Olayode@users.noreply.github.com>
2024-07-09 15:29:29 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,45 @@
+
+var intro = document.querySelector('.h1'); 
+h1.setAttribute('class', 'Olayode Helen Victoria'); 
+
+
+const terms = document.createElement('p'); 
+terms.textContent = "victoriaolayode1@gmail.com"; 
+terms.setAttribute('id', 'vicki'); 
+terms.setAttribute('class', 'Class-s-m-x'); 
+
+const terms = document.createElement('p'); 
+terms.textContent = "victoriaolayode1@gmail.com"; 
+terms.setAttribute('id', 'vicki'); 
+terms.setAttribute('class', 'Class-s-m-x');
+
+
+const date = new Date();
+
+let day = date.getDate();
+let month = date.getMonth() + 1;
+let year = date.getFullYear();
+
+var currentDate = date.format('D/MM/YYYY');
+console.log(currentDate); // "10/07/2024"
+
+var a = document.createElement('a');
+      var linkText = document.createTextNode("hngLink");
+      a.appendChild(hngLink);
+      a.title = "hngLink";
+      a.href = "http://hng.tech/learn";
+      document.body.appendChild(a);
+
+var a = document.createElement('a');
+      var linkText = document.createTextNode("keywordLink");
+      a.appendChild(keywordLink);
+      a.title = "keywordLink";
+      a.href = "http://keyword.dog/";
+      document.body.appendChild(a);
+
+var a = document.createElement('a');
+      var linkText = document.createTextNode("scrapeanywebLink");
+      a.appendChild(scrapeanywebLink);
+      a.title = "scrapeanywebLink";
+      a.href = "http://scrapeanyweb.site/";
+      document.body.appendChild(a);
